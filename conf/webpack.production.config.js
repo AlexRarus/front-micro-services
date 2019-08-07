2873f1d68712a166101bd00ef7089a555e035036
Серебряков Алексей <aserebryakov@roox.ru>
2019-08-07 11:40:15 +0300

    Automatic commit of successful build

--- conf/webpack.production.config.js
+++ conf/webpack.production.config.js
@@ -1,6 +1,6 @@
 import Config from 'webpack-config';
 
-export default new Config().extend('src/webpack.base.config.js').merge({
+export default new Config().extend('conf/webpack.base.config.js').merge({
   output: {
     filename: 'bundle.min.[hash:8].js'
   },
